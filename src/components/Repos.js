8f46f775a42d618dbb561c0305471d319fc68b74
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-06-27 20:25:04 -0600

    plugged in all graphs to mock data... next will enable actual data to be fetched!

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -31,15 +31,26 @@ const Repos = () => {
   }).map((item) => {
     return {...item, value:item.stars}
   }).slice(0, 5);
-  console.log(mostPopular);
+
+  let {stars, forks} = repos.reduce((total, item) => {
+    const {stargazers_count, name, forks} = item;
+    total.stars[stargazers_count] = {label: name, value: stargazers_count}
+    total.forks[forks] = {label: name, value: forks};
+    return total;
+  }, {
+    stars:{}, forks:{}
+  });
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
 
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <Column3D data={mostUsed} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar3D data={mostUsed}/>
+        <Bar3D data={forks}/>
       </Wrapper>
     </section>
   )
