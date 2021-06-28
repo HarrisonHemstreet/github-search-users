74404f279c0896b39e0370c94afbca9b521d1c86
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-06-27 18:43:12 -0600

    added example chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,7 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const {repos} = React.useContext(GithubContext);
   return (
-<h1>hi</h1>
+    <ExampleChart />
   )
 };
 
