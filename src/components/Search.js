c24c2a2e9ef4a594a6b6c1565de45347e3e2ff61
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-06-27 20:58:54 -0600

    requests are now able to be tracked

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,8 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user, setUser] = React.useState('');
+  const {requests} = React.useContext(GithubContext);
+  console.log(requests, "🤡🤡🤡🤡");
   // get things from global context
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,10 +18,10 @@ const Search = () => {
           <div className="form-control">
             <MdSearch />
             <input type="tet" placeholder="enter github user" value={user} onChange={(e) => setUser(e.target.value)} />
-            <button type="submit" >search</button>
+            {requests > 0 && <button type="submit" >search</button> }
           </div>
         </form>
-        <h3>requests : 60 / 60</h3>
+        <h3>requests : {requests} / 60</h3>
       </Wrapper>
     </section>
   )
